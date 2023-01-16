900f8f65eb75570bfb716d6ac2ab1a27452d7dbe
Michel Silva <michel_silva_04@hotmail.com>
2023-01-16 11:52:36 -0300

    Collection Test

--- Michel/PriceDoubleCheck/Test/Unit/Model/ResourceModel/PriceApprove/CollectionTest.php
+++ Michel/PriceDoubleCheck/Test/Unit/Model/ResourceModel/PriceApprove/CollectionTest.php
@@ -1,17 +1,81 @@
 <?php
 
-namespace Michel\PriceDoubleCheck\Test\Unit\Model\ResourceModel;
+namespace Michel\PriceDoubleCheck\Test\Unit\Model\ResourceModel\PriceApprove;
 
-use Michel\PriceDoubleCheck\Api\Data\PriceApproveInterface;
-use Michel\PriceDoubleCheck\Model\ResourceModel\PriceApprove;
+use Magento\Framework\Api\Search\AggregationInterface;
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
+use Magento\Framework\Data\Collection\EntityFactoryInterface;
 use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Select;
+use Magento\Framework\EntityManager\MetadataPool;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Store\Model\StoreManagerInterface;
+use Michel\PriceDoubleCheck\Model\ResourceModel\PriceApprove\Collection;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 
-class PriceApproveTest extends TestCase
+class CollectionTest extends TestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->entityFactoryMock = $this->getMockBuilder(EntityFactoryInterface::class)
+            ->getMockForAbstractClass();
+        $this->loggerMock = $this->getMockBuilder(LoggerInterface::class)
+            ->getMockForAbstractClass();
+        $this->fetchStrategyMock = $this->getMockBuilder(FetchStrategyInterface::class)
+            ->getMockForAbstractClass();
+        $this->eventManagerMock = $this->getMockBuilder(ManagerInterface::class)
+            ->getMockForAbstractClass();
+        $this->storeManagerMock = $this->getMockBuilder(StoreManagerInterface::class)
+            ->getMockForAbstractClass();
+        $this->metadataPoolMock = $this->getMockBuilder(MetadataPool::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->resourceMock = $this->getMockBuilder(AbstractDb::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->aggregationsMock = $this->getMockBuilder(AggregationInterface::class)
+            ->getMockForAbstractClass();
+        $this->connectionMock = $this->getMockBuilder(AdapterInterface::class)
+            ->getMockForAbstractClass();
+        $this->selectMock = $this->getMockBuilder(Select::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->resourceMock->expects($this->any())
+            ->method('getConnection')
+            ->willReturn($this->connectionMock);
+        $this->resourceMock->expects($this->any())->method('getMainTable')->willReturn('price_approve');
+
+        $this->connectionMock->expects($this->once())
+            ->method('select')
+            ->willReturn($this->selectMock);
+
+
+        $this->collection = (new ObjectManager($this))->getObject(Collection::class, [
+            'entityFactory' => $this->entityFactoryMock,
+            'logger' => $this->loggerMock,
+            'fetchStrategy' => $this->fetchStrategyMock,
+            'eventManager' => $this->eventManagerMock,
+            'storeManager' => $this->storeManagerMock,
+            'metadataPool' => $this->metadataPoolMock,
+            'mainTable' => null,
+            'eventPrefix' => 'test_event_prefix',
+            'eventObject' => 'test_event_object',
+            'resourceModel' => null,
+            'resource' => $this->resourceMock,
+        ]);
     }
 
+    /**
+     * @return void
+     */
+    public function testCollection()
+    {
+        $this->assertInstanceOf(Collection::class, $this->collection);
+    }
 }
